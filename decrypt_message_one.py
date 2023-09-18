f69a5130647cd748b4643e0d4da6f52171841ffe
Jiayue Tang <tjiayue2002@gmail.com>
2023-09-18 10:41:49 -0400

    Updated Decrypted message one

--- decrypt_message_one.py
+++ decrypt_message_one.py
@@ -34,10 +34,20 @@ cipher = {
     '.': '%' 
 }
 
-encrypted_file = open("encrypted_message_one.txt", 'r')
+encrypted_file = open("c:/Users/tjiay/OneDrive/Desktop/Y3S1 (On Exchange)/UConn/STAT4185/STAT_4185_Assignment_1/encrypted_message_one.txt", 'r')
 
 encrypted_message = encrypted_file.readline()
 
 encrypted_file.close()
 
 # Write code below
+
+decrypted = ''
+for c in encrypted_message:
+    for key, value in cipher.items():
+        if str(value) == c:
+            decrypted += key
+            
+   
+print(decrypted)
+
