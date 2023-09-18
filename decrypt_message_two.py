9f03e13ff4e76944fde7a54f7e04361e16cc970e
Jiayue Tang <tjiayue2002@gmail.com>
2023-09-18 11:01:47 -0400

    modified decrypt_message_two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,18 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+def decrypt_message(encrypted):
+    msg = list(encrypted)
+    s, e = 1, len(msg) - 2  # Adjusted for zero-based indexing
+
+    while s < e:
+        msg[s], msg[e] = msg[e], msg[s]
+        s, e = s + 2, e - 2
+
+    return "".join(msg)
+
+# Decrypt the message
+decrypted = decrypt_message(encrypted_message)
+
+# Print the decrypted message
+print(decrypted)
